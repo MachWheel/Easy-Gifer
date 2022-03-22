ce99c804e2158a78694e240f887a83e43ac667fd
wyller <wyller22@gmail.com>
2022-03-21 22:34:54 -0300

    Converted graphics to base64

---
+++ resources/labels.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+APP_TITLE = 'Easy Gifer'
+START_TOOLTIP = "Make a gif"
+BROWSE_TOOLTIP = "Choose a video"
+SELECT_VIDEO = 'Choose any video file to convert into a gif:'
+TRIM = "Trim?"
+START_AT = 'Start at:'
+DURATION = 'Duration:'
+SPEED_DISPLAY = '1.0x'
+CHANGE_SPEED = 'Change speed:'
