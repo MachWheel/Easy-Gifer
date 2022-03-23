0510facb55ab14c0ee6c9a39bc43edc748ea054c
wyller <wyller22@gmail.com>
2022-03-23 01:07:37 -0300

    Fixed compiler script

--- compile/compile.bat
+++ compile/compile.bat
@@ -2,10 +2,18 @@
 :: This batch file compiles MessyFolderOrganizer application
 :: The generated app is in the ./dist folder
 :: First make sure to "pip install -r requirements.txt"
-TITLE Compiling MessyFolderOrganizer...
+TITLE Easy-Gifer Compiler
+ECHO:
+ECHO Easy_Gifer Compiler v1
+ECHO:
+ECHO PRESS ANY KEY TO START COMPILING
+ECHO:
+PAUSE
+MKDIR dist
+ROBOCOPY ".." "dist" ffmpeg.exe /mt /z
 pyinstaller --onefile -w compile.spec
 ECHO:
-ECHO DONE! PRESS ANYTHING TO OPEN OUTPUT FOLDER
+ECHO DONE! PRESS ANY KEY TO OPEN THE OUTPUT FOLDER.
 ECHO:
 PAUSE
 START dist
